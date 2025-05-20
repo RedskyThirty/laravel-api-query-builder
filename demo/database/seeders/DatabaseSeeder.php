37a89e7324d1bb7404e72ec12c2178b337dd7db4
Manuel Piton <manu@redsky.be>
2025-05-20 09:17:48 +0200

    feat(seeders): create demo data

--- demo/database/seeders/DatabaseSeeder.php
+++ demo/database/seeders/DatabaseSeeder.php
@@ -2,22 +2,35 @@
 
 namespace Database\Seeders;
 
+use App\Models\Address;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\Profile;
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+class DatabaseSeeder extends Seeder {
+	/**
+	 * Seed the application's database.
+	 */
+	public function run(): void {
+		User::factory(20)->create()->each(function (User $user) {
+			$user->profile()->save(
+				Profile::factory()->make()
+			);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+			$addresses = Address::factory(2)->make();
+			$user->addresses()->saveMany($addresses);
+
+			$user->posts()->saveMany(
+				Post::factory(rand(2, 6))->make()
+			)->each(function (Post $post) {
+				$post->comments()->saveMany(
+					Comment::factory(rand(2, 10))->make()
+				);
+			});
+		});
+	}
 }
