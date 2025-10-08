c7eae8d9d5e54b28543141b17d74b4bcd89792bc
Manuel Piton <manu@redsky.be>
2025-10-08 15:19:09 +0200

    feat(database-seeder): create unverfified users

--- demo/database/seeders/DatabaseSeeder.php
+++ demo/database/seeders/DatabaseSeeder.php
@@ -11,26 +11,43 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+/**
+ * Class DatabaseSeeder
+ *
+ * @package Database\Seeders
+ */
 class DatabaseSeeder extends Seeder {
 	/**
 	 * Seed the application's database.
 	 */
 	public function run(): void {
-		User::factory(34)->create()->each(function (User $user) {
-			$user->profile()->save(
-				Profile::factory()->make()
+		User::factory(8)->unverified()->create()->each(function (User $user) {
+			$this->finalizeUser($user);
+		});
+
+		User::factory(26)->create()->each(function (User $user) {
+			$this->finalizeUser($user);
+		});
+	}
+
+	/**
+	 * @param User $user
+	 * @return void
+	 */
+	private function finalizeUser(User $user): void {
+		$user->profile()->save(
+			Profile::factory()->make()
+		);
+
+		$addresses = Address::factory(2)->make();
+		$user->addresses()->saveMany($addresses);
+
+		$user->posts()->saveMany(
+			Post::factory(rand(2, 6))->make()
+		)->each(function (Post $post) {
+			$post->comments()->saveMany(
+				Comment::factory(rand(2, 10))->make()
 			);
-
-			$addresses = Address::factory(2)->make();
-			$user->addresses()->saveMany($addresses);
-
-			$user->posts()->saveMany(
-				Post::factory(rand(2, 6))->make()
-			)->each(function (Post $post) {
-				$post->comments()->saveMany(
-					Comment::factory(rand(2, 10))->make()
-				);
-			});
 		});
 	}
 }
